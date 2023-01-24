85af1f909550e0e9fbf6dfb4a061def11c962ef5
aritroCoder <ogcodinggod@gmail.com>
2023-01-25 01:08:13 +0530

    gas fee snap

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,11 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
 
+async function getFees() {
+  const response = await fetch('https://beaconcha.in/api/v1/execution/gasnow');
+  const parse = await response.json()
+  return JSON.stringify(parse.data);
+}
+
 /**
  * Get a message from the origin. For demonstration purposes only.
  *
@@ -23,17 +29,23 @@ export const getMessage = (originString: string): string =>
 export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
   switch (request.method) {
     case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: getMessage(origin),
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
+      return getFees().then((fees) => {
+        const feesObject = JSON.parse(fees);
+        const baseFee = parseFloat(feesObject.currentBaseFee);
+        const safeLow = Math.ceil(baseFee + parseFloat(feesObject.safeLow));
+        const standard = Math.ceil(baseFee + parseFloat(feesObject.standard));
+        const fastest = Math.ceil(baseFee + parseFloat(feesObject.fastest)); 
+
+        return wallet.request({
+          method: 'snap_confirm',
+          params: [
+            {
+              prompt: getMessage(origin),
+              description: 'Current Gas Fees from etherchain.org:',
+              textAreaContent: `Current gas estimates: ${fees}`,
+            },
+          ],
+        });
       });
     default:
       throw new Error('Method not found.');
